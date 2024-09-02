57faccc814e7c0290f1a4a10f0269583ad6ee2c6
Antony-ant777 <antonychris2004@gmail.com>
2024-09-03 01:02:03 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,117 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+
+import { CiSearch } from "react-icons/ci";
+import { FaHome, FaThumbsUp } from "react-icons/fa";
+import { GiBackwardTime } from "react-icons/gi";
+import { GrGallery } from "react-icons/gr";
+import { HiMenu } from "react-icons/hi";
+import { IoLogoYoutube } from "react-icons/io";
+import { IoTimeSharp } from "react-icons/io5";
+import { MdOutlineLiveTv } from "react-icons/md";
+import { RiAccountCircleFill, RiMenuUnfold3Fill } from "react-icons/ri";
+import { TbLogin } from "react-icons/tb";
+
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div>
+      <div className='leftside' >
+
+        <HiMenu  className='youtubeic'/><br></br>
+        <FaHome  className='youtubeic'/><br></br>
+        <GrGallery className='youtubeic' /><br></br>
+        <GiBackwardTime  className='youtubeic'/><br></br>
+        <RiMenuUnfold3Fill  className='youtubeic'/><br></br>
+        <MdOutlineLiveTv className='youtubeic'/><br></br>
+        <IoTimeSharp className='youtubeic'/><br></br>
+        <FaThumbsUp  className='youtubeic'/><br></br>
+      
+      </div>
+      
+      <div className='head'>
+        
+      <IoLogoYoutube className='youtube'/>
+      <input type='text' className='input' placeholder='Search'></input>
+      <CiSearch className='Search'/>
+      <div className='topbar'>
+      <RiAccountCircleFill  className='login'/>
+      <TbLogin className='logout'/>
+      
+      </div>
+       </div>
+       <div className='content'>
+        <div className='video'>
+        <iframe width="300" height="180" src="https://www.youtube.com/embed/qu7fef0JzHc?si=0VVhzmczQVVVHr1M" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+        <p>Kattu Kuyilu Video Song | Thalapathi Tamil Movie Songs | Rajanikanth,Mammootty |Maniratnam|Ilayaraja</p>
+        <p>26,972,278 views  17 May 2012</p>
+
+        </div>
+        <div className='video'>
+        <iframe width="300" height="180" src="https://www.youtube.com/embed/Ja168gMpb3o?si=4eG7PsJ5DsvyrkYm" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+         <p>3 - Why This Kolaveri Di Video | Dhanush, Shruti | Anirudh</p>
+        <p>60,925,763 views  10 Sept 2014</p>
+
+        </div>
+        <div className='video'>
+        <iframe width="300" height="180" src="https://www.youtube.com/embed/PnV0RQ96XU4?si=NvbH5Urwudnb1drL" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+         <p>Venmathi Venmathiye Official Video | Minnale | Harris Jayaraj | Madhavan | Gautham V. Menon</p>
+        <p>40,153,921 views  28 Aug 2021</p>
+
+        </div>
+
+       </div>
+       <div className='content'>
+        <div className='video'>
+        <iframe width="300" height="180" src="https://www.youtube.com/embed/Xt-RpOfOxDU?si=MjBjQD5BvwiCRveh" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+        <p>7 Aum Arivu - Yamma Yamma Tamil Tamil Lyric | Suriya | Harris</p>
+        <p>16,356,238 views  29 Nov 2017</p>
+
+        </div>
+        <div className='video'>
+        <iframe width="300" height="180" src="https://www.youtube.com/embed/HeAirHnCzok?si=CTSupCIGRAFhDQGd" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+         <p>Kannadasan Karaikudi - 4K Video Song|கண்ணதாசன் காரைக்குடி|Anjathe | Naren | Mysskin | Sundar C Babu</p>
+        <p> 9,069,111 views  6 Apr 2024</p>
+
+        </div>
+        <div className='video'>
+        <iframe width="300" height="180" src="https://www.youtube.com/embed/K_paY-RznA0?si=nAUSd4rTiHNlfBtc" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+        <p>Alwarpetai Aaluda 4K Video Song | ஆழ்வார்பேட்டை ஆளுடா | Vasool Raja | Kamal Haasan | Bharadwaj</p>
+        <p>9,765,618 views  7 Nov 2023</p>
+
+        </div>
+
+       </div>
+       <div className='content'>
+        <div className='video'>
+        <iframe width="300" height="180" src="https://www.youtube.com/embed/p7_GDN6r2OY?si=urBRSoRVVjaawI0E" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+        <p>Oruvan Oruvan - 4K Video Song | Superstar Rajinikanth | A R Rahman | Muthu | Tamil Song | Sun Music</p>
+        <p>4.2M views  5 months ago</p>
+
+        </div>
+        <div className='video'>
+        <iframe width="300" height="180" src="https://www.youtube.com/embed/WxtCoyRsN4k?si=sMTJKerBit7jDZ2c" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+        <p>Aye Aye Aye - Official Video Song | Aambala | Vishal,Hansika | Sundar C | Hiphop Tamizha</p>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        19M views  9 years ago</p>
+
+        </div>
+        <div className='video'>
+        <iframe width="300" height="180" src="https://www.youtube.com/embed/7mWd8fOG7qI?si=8zOAl8b0W6Iw6baX" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+        <p>Vaadi Pulla Vaadi - Video Song | Meesaya Murukku | Hiphop Tamizha | Vivek | Sundar.C</p>
+        <p>154M views  7 years ago</p>
+
+        </div>
+
+       </div>
+
+
+
+
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
